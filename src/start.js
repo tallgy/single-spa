41e1c80b5e1d881424e47843b64f1567375862b9
tallgy <1776232657@qq.com>
2023-08-14 00:56:01 +0800

    start

--- src/start.js
+++ src/start.js
@@ -5,6 +5,10 @@ import { isInBrowser } from "./utils/runtime-environment.js";
 
 let started = false;
 
+/**
+ * 内部调用的还是 reroute 方法来开始启动
+ * @param {*} opts 
+ */
 export function start(opts) {
   started = true;
   if (opts && opts.urlRerouteOnly) {
@@ -15,6 +19,11 @@ export function start(opts) {
   }
 }
 
+/**
+ * 代表是不是已经调用了 start 调用 start方法第一步就是设置为 true
+ * 所以第一次调用 start，在 start 里面，获取 isStarted 也会变成 true 了。
+ * @returns { boolean }
+ */
 export function isStarted() {
   return started;
 }
